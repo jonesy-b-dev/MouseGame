bcbf74325cd106deab878859df741b8eed9990e5
Jonas de Bruin <jonasdebruin@proton.me>
2024-09-27 09:39:06 +0200

    Few more definetions for the player

--- Source/Game/Player/Player.h
+++ Source/Game/Player/Player.h
@@ -16,7 +16,12 @@ private:
 	// Gameplay variables
 public:
 private:
+	bool m_isSafe = true;
+	float m_hunger = 100;
 	float m_speed = 140;
 	float m_health = 100;
+	int m_foodinventory = 0;
+	void Death();
+	void Eat();
 	void HandleInput();
 };

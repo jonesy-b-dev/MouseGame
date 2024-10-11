e13d1ebb540b73d7aae81ee85740014247e07e56
Jonas de Bruin <jonasdebruin@proton.me>
2024-10-11 11:44:38 +0200

    Changed way the vector math works

--- Source/Engine/Vector2/Vector2.cpp
+++ Source/Engine/Vector2/Vector2.cpp
@@ -1,21 +1,21 @@
 #include "Vector2.h"
 
-Engine::Vector2 Engine::Vector2::Add(Vector2 one, Vector2 two)
+Engine::Vector2 Engine::Vector2::Add(Vector2 two)
 {
-	return Vector2(one.x + two.x, one.y + two.y);
+	return Vector2(x + two.x, y + two.y);
 }
 
-Engine::Vector2 Engine::Vector2::Subtract(Vector2 one, Vector2 two)
+Engine::Vector2 Engine::Vector2::Subtract(Vector2 two)
 {
-	return Vector2(one.x - two.x, one.y - two.y);
+	return Vector2(x - two.x, y - two.y);
 }
 
-Engine::Vector2 Engine::Vector2::Multiply(Vector2 one, Vector2 two)
+Engine::Vector2 Engine::Vector2::Multiply(Vector2 two)
 {
-	return Vector2(one.x * two.x, one.y * two.y);
+	return Vector2(x * two.x, y * two.y);
 }
 
-Engine::Vector2 Engine::Vector2::Divide(Vector2 one, Vector2 two)
+Engine::Vector2 Engine::Vector2::Divide(Vector2 two)
 {
-	return Vector2(one.x / two.x, one.y / two.y);
+	return Vector2(x / two.x, y / two.y);
 }
--- Source/Engine/Vector2/Vector2.h
+++ Source/Engine/Vector2/Vector2.h
@@ -7,9 +7,9 @@ namespace Engine
 		float x, y;
 		Vector2(float x, float y) : x(x), y(y) {}
 		Vector2() {};
-		Vector2 Add(Vector2 one, Vector2 two);
-		Vector2 Subtract(Vector2 one, Vector2 two);
-		Vector2 Multiply(Vector2 one, Vector2 two);
-		Vector2 Divide(Vector2 one, Vector2 two);
+		Vector2 Add(Vector2 two);
+		Vector2 Subtract(Vector2 two);
+		Vector2 Multiply(Vector2 two);
+		Vector2 Divide(Vector2 two);
 	};
 }
